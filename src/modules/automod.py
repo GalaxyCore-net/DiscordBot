b7e61fc18fc89384cf1ede4423acbe36302c69d0
Flo-Mit-H <flogamesg@gmail.com>
2021-08-02 18:28:48 +0200

    - Added Automod

---
+++ src/modules/automod.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+
+from bot import config, logger
+
+
+class Automod(commands.Cog):
+    def __init__(self, _bot):
+        self.bot = _bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        await self.process_message(message)
+        await message.delete()
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before: discord.Message, after: discord.Message):
+        await self.process_message(after)
+        await after.edit(content=before.content, embed=before.embed)
+
+    @staticmethod
+    async def process_message(message: discord.Message):
+        for bad_word in config["automod"]:
+            if bad_word in message.content.lower():
+                logger.info(f"{message.author.name} wrote the forbidden word '{bad_word}'")
+                await message.author.send(content="Du hast ein schlimmes Wort geschrieben!\n"
+                                                  "Genauer Wortlaut: \n"
+                                                  f"{message.content}\n"
+                                                  "Bitte unterlasse das!")
+
+
+def setup(_bot):
+    _bot.add_cog(Automod(_bot))
