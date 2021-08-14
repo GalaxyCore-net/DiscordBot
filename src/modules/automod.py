382f82dd3e1d410b1f2167b0723ccf1850ed58ba
Flo-Mit-H <flogamesg@gmail.com>
2021-08-14 11:22:39 +0200

    - Add add-insult command and fix some bugs in automod

--- src/modules/automod.py
+++ src/modules/automod.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import has_role, has_permissions
 
 from bot import logger
 
@@ -25,18 +26,38 @@ class Automod(commands.Cog):
 
     @staticmethod
     async def process_message(message: discord.Message):
+        if message.author.bot:
+            return
         session = sessionmaker(bind=engine, autocommit=True, autoflush=True)()
         session.begin()
         words = session.query(BadWordsModel).all()
         for bad_word in words:
             if bad_word.word in message.content.lower():
-                logger.info(f"{message.author.name} wrote the forbidden word '{bad_word}'")
+                logger.info(f"{message.author.name} wrote the forbidden word '{bad_word.word}'")
                 await message.author.send(content="Du hast ein schlimmes Wort geschrieben!\n"
                                                   "Genauer Wortlaut: \n"
                                                   f"{message.content}\n"
                                                   "Bitte unterlasse das!")
                 return True
         return False
+
+    @has_permissions(administrator=True)
+    @commands.command(aliases=["add-insult"])
+    async def add_insult(self, ctx, *, insult):
+        session = sessionmaker(bind=engine, autocommit=True, autoflush=True)()
+        session.begin()
+        new_word = BadWordsModel(word=insult)
+        session.add(new_word)
+        session.commit()
+        session.flush()
+        session.close()
+
+        embed = discord.Embed()
+        embed.title = "Wort hinzugefügt!"
+        embed.description = f"Das Word {insult} wurde erfolgreich hinzugefügt!"
+        embed.colour = discord.Color(0x00ff00)
+
+        ctx.send(embed=embed)
 
 
 def setup(_bot):
